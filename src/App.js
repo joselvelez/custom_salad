6fa735ba40d9ccb5f2831c134b606e0539e162ae
joselvelez <joselvelez@protonmail.com>
2021-10-22 19:28:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import SaladMaker from './components/SaladMaker/SaladMaker';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <SaladMaker />
+    </>
+  );
+}
+
+export default App;
